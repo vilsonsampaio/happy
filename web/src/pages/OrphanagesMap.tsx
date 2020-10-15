e75d8059d08f2224d048b5528342187d878df85d
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-10-15 01:17:26 -0300

    :sparkles: feat: copy missing pages and icrement navigation among them

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -58,14 +58,14 @@ function OrphanagesMap() {
             className="map-popup"
           >
             Lar das meninas
-            <Link to="">
+            <Link to="/orphanages/1">
               <FiArrowRight size={20} color="#FFF" />
             </Link>
           </Popup>
         </Marker>
       </Map>
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
       </Link>
     </div>
