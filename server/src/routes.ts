fab83a88823a315e2c29f96d9698405d30cfb043
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-10-21 22:40:45 -0300

    :sparkles: feat: add user's sign in

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,12 @@
-import { Router } from 'express';
+import { response, Router } from 'express';
 import multer from 'multer';
 
 import uploadConfig from './config/upload';
 
 import OrphanagesController from './controllers/OrphanagesController';
 import UsersController from './controllers/UsersController';
+
+import authMiddleware from './middlewares/auth';
 
 const routes = Router();
 const upload = multer(uploadConfig);
@@ -14,5 +16,8 @@ routes.get('/orphanages/:id', OrphanagesController.show);
 routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
 
 routes.post('/users', UsersController.create);
+routes.get('/users', UsersController.show);
+
+routes.get('/auth', authMiddleware, (request, response) => response.status(200).json({ ok: true }));
 
 export default routes;
