d8c7177131170eff61b6402a8b9f738805efcab3
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-10-28 00:03:54 -0300

    :bento: chore: export images as React Component

---
+++ web/src/assets/images/index.tsx
@@ -0,0 +1,3 @@
+export { ReactComponent as Happy } from './logo.svg';
+export { ReactComponent as LandingIllustration } from './landing.svg';
+export { ReactComponent as MapMarker } from './map-marker.svg';
