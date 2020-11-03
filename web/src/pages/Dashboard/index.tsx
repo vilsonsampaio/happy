bf312356923d30a53b5ce4f7065702fc971d6fa2
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-11-02 22:03:10 -0300

    :alien: chore: update dashboard page

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -6,6 +6,8 @@ import { MapMarker, AlertCircleNotification, NoOrphanagesIcon } from '../../asse
 
 import { mapIcon } from '../../utils';
 
+import useAuth from '../../hooks/useAuth';
+
 import api from '../../services/api';
 
 import { Container, Siderbar, SidebarButton, Main, MainWrapper, Cards, Card, CardButton, NoOrphanages } from './styles';
@@ -22,12 +24,14 @@ type CurrentPageProps = "approved" | "pending";
 let initialPage: CurrentPageProps = "approved";
 
 const Dashboard: React.FC = () => {
+  const { handleSignOut } = useAuth();
+
   const [currentPage, setCurrentPage] = useState<CurrentPageProps>(initialPage);
 
   const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
 
   const [pendingOrphanages, setPendingOrphanages] = useState<Orphanage[]>([]);
-  const [approvedOrphanages, setApprovedOrphanages] = useState<Orphanage[]>([]);
+  const [, setApprovedOrphanages] = useState<Orphanage[]>([]);
 
   // Initialing arrays
   useEffect(() => {
@@ -59,34 +63,31 @@ const Dashboard: React.FC = () => {
     ;
 
     // Getting pending orphanages
-    // Change to axios get, when authentication is done.
-    let initialPendingOrphanages = [
-      { 
-        id: 3, 
-        name: 'Affs vidah', 
-        latitude: -12.7168259, 
-        longitude: -38.3282798
-      },
-      { 
-        id: 4, 
-        name: 'Criança feliz', 
-        latitude: -12.7168259, 
-        longitude: -38.3282798
-      },
-      { 
-        id: 5, 
-        name: 'Oi vidinha', 
-        latitude: -12.7168259, 
-        longitude: -38.3282798
-      },
-    ];
+    api
+      .get<Orphanage[]>('orphanages-pending')
+      .then(response => {
+        const serializedOrphanages: Orphanage[] = response.data
+          .map(orphanage => {
+            return {
+              id: orphanage.id,
+              name: orphanage.name,
+              latitude: orphanage.latitude,
+              longitude: orphanage.longitude,
+            }
+          })
+        ;
 
-    setPendingOrphanages(initialPendingOrphanages);
+        setPendingOrphanages(serializedOrphanages);
 
-    if (initialPage === 'pending') {
-      // Initialing the default orphanage array as the pending one's.
-      setOrphanages(initialPendingOrphanages);
-    }
+        if (initialPage === 'pending') {
+          // Initialing the default orphanage array as the pending one's.
+          setOrphanages(serializedOrphanages);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      })
+    ;
   }, []);
 
   useEffect(() => {
@@ -115,19 +116,29 @@ const Dashboard: React.FC = () => {
         })
       ;
     } else {
-      // Change to axios get, when authentication is done.
-      setPendingOrphanages((prevState) => {
-        const newPendingOrphanage: Orphanage = {
-          id: prevState[prevState.length -1].id * 10,
-          name: prevState[0].name,
-          latitude: prevState[0].latitude,
-          longitude: prevState[0].longitude,
-        };
+      api
+        .get<Orphanage[]>('orphanages-pending')
+        .then(response => {
+          const serializedOrphanages: Orphanage[] = response.data
+            .map(orphanage => {
+              return {
+                id: orphanage.id,
+                name: orphanage.name,
+                latitude: orphanage.latitude,
+                longitude: orphanage.longitude,
+              }
+            })
+          ;
 
-        setOrphanages([...prevState, newPendingOrphanage]);
+          setPendingOrphanages(serializedOrphanages);
 
-        return [...prevState, newPendingOrphanage];
-      });
+          // Change the default orphanage array to the pending one's.
+          setOrphanages(serializedOrphanages);
+        })
+        .catch(error => {
+          console.error(error);
+        })
+      ;
     }
   }, [currentPage]);
   
@@ -159,7 +170,7 @@ const Dashboard: React.FC = () => {
           </SidebarButton>
         </div>
 
-        <SidebarButton type="button">
+        <SidebarButton type="button" onClick={handleSignOut}>
           <FiPower />
         </SidebarButton>
       </Siderbar>
