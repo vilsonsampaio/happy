6d9daadb8a0336f6acad41c6edbef62203c5b835
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-11-04 21:20:57 -0300

    :card_file_box: feat: delete images where preview_images doesn't matches with its paths

--- server/src/controllers/OrphanagesController.ts
+++ server/src/controllers/OrphanagesController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import url from 'url';
 import { getRepository } from 'typeorm';
 import * as Yup from 'yup';
 
@@ -6,6 +7,8 @@ import Orphanage from '../models/Orphanage';
 import Image from '../models/Image';
 
 import orphanageView from '../views/orphanages_view';
+import imagesView from '../views/images_view';
+
 
 export default {
   async index(request: Request, response: Response) {
@@ -134,6 +137,14 @@ export default {
 
     const requestImages = request.files as Express.Multer.File[];
 
+    let preview_images = request.body.preview_images;
+
+    if (typeof preview_images === 'string') {
+      preview_images = [preview_images];
+    } else {
+      preview_images = [...preview_images];
+    }
+
     const images = requestImages.map((image) => {
       return {
         path: image.filename,
@@ -166,24 +177,40 @@ export default {
         Yup.object().shape({
           orphanage: Yup.object().required().shape({
             id: Yup.number().required(),
-          }),
+          }).required(),
           path: Yup.string().required(),
-        })
-      ),
+        }).required()
+      ).notRequired(),
+      preview_images: Yup.array(
+        Yup.string().required()
+      ).required(),
       is_pending: Yup.boolean().required(),
     });
 
-    await schema.validate({ ...data, images }, {
+    await schema.validate({ ...data, images, preview_images }, {
       abortEarly: false,
     });
 
     const imagesRepository = getRepository(Image);
 
-    await imagesRepository.delete({ orphanage: { id } });
+    const imagesDB = await imagesRepository.find({ orphanage: { id } });
+    
+    const imagesPathDB = imagesView.renderMany(imagesDB)?.map(imageDB => imageDB.url);
+
+    const imagesToDelete = imagesPathDB.filter(image => !preview_images.includes(image));
+
+    if (imagesToDelete.length > 0) {
+      await Promise.all(imagesToDelete.map(async (imageToDelete) => {
+        const path = url.parse(imageToDelete)?.path?.replace('/uploads/', '');
+
+        await imagesRepository.delete({ path, orphanage: { id }});
+      }));
+    }
+    
     await imagesRepository.insert(images);
     
     const orphanagesRepository = getRepository(Orphanage);
-
+      
     await orphanagesRepository.update(id, data);
 
     return response.status(200).send();
